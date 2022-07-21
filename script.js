a84ce4eaebfa790e7f23c67566228d4fd549ebbc
Lord-Kennus <kennedy-a40@ulster.ac.uk>
2022-07-21 03:24:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,11 @@
 var db = firebase.firestore();
 var dbRef = firebase.database().ref().child('text');
 const Count = 1;
-const PLACES;
-const loadPlaces = function (coords) {
-    ///*
-    const method = 'static';
-    if (method === 'api') {
-        return loadPlaceFromAPIs(coords);
-    }
-    //*/
-    return loadPlaceStatic();
-};
+
 
 function loadPlaceStatic() {
     
-    PLACES = [
+    const PLACES = [
         {
             name: 'Hole 1',
             location: {
@@ -67,67 +58,31 @@ function loadPlaceStatic() {
         }
     })
 }
-/*
-function resetPlaces(){
-    if(PLACES.length > 0){
-        for(const i = 0; i > PLACES.length; i++){
-            PLACES.shift();
-        }
-    }
-}
 
-function nextHole(){
-    scene.parentElement.removeChild(scene);
-    resetPlaces();
-    Count++;
-    alert('Hole:',Count);
-    if (Count <= 18){  // Future change, Count <= Hole 
-        alert('You are finished!');
-    }
-    else{
-        renderPlaces(places);
-    }
-}
+const loadPlaces = function (coords) {
+    // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
+    const method = 'static';
 
-function previousHole(){
-    scene.parentElement.removeChild(scene);
-    resetPlaces();
-    Count--;
-    alert('Hole', Count);
-    if(Count < 1){
-        alert('There is no previous hole!');
-        Count = 1;
+    if (method === 'api') {
+        return loadPlaceFromAPIs(coords);
     }
-    else{
-        renderPlaces(places);
-    }
-}
-/*
-const calcDist = function(lat2, lon2){
-    const R = 6371e3; 
-    const φ1 = position.coords.latitude * Math.PI/180;
-    const φ2 = position.coords.longitude * Math.PI/180;
-    const Δφ = (lat2-lat1) * Math.PI/180;
-    const Δλ = (lon2-lon1) * Math.PI/180;
 
-    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-          Math.cos(φ1) * Math.cos(φ2) *
-          Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return loadPlaceStatic();
+};
 
-    const d = R * c;
-    return d;
-} 
-//*/
-///*
+// getting places from REST APIs
 function loadPlaceFromAPIs(position) {
     const params = {
-        radius: 300,    
+        radius: 300,    // search places not farther than this value (in meters)
         clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
         clientSecret: 'GYRKWWJMO2WK3KIRWBXIN5FQAWXTVFIK2QM4VQWNQ4TRAKWH',
-        version: '20300101',   
+        version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
+
+    // CORS Proxy to avoid CORS problems
     const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+    // Foursquare API
     const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
         &ll=${position.latitude},${position.longitude}
         &radius=${params.radius}
@@ -146,10 +101,15 @@ function loadPlaceFromAPIs(position) {
             console.error('Error with places API', err);
         })
 };
-//*/
+
+
 window.onload = () => {
     const scene = document.querySelector('a-scene');
+
+    // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
+
+        // than use it to load from remote APIs some places nearby
         loadPlaces(position.coords)
             .then((places) => {
                 alert(position.coords.latitude + " : " + position.coords.longitude);
@@ -157,39 +117,17 @@ window.onload = () => {
                     const latitude = place.location.lat;
                     const longitude = place.location.lng;
 
+                    // add place name
                     const text = document.createElement('a-link');
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     text.setAttribute('title', place.name);
-                    text.setAttribute('scale', '6 6 6');
+                    text.setAttribute('href', 'https://akqa.com/');
+                    text.setAttribute('scale', '13 13 13');
 
                     text.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
                     });
 
-                    /*  Click Event for Distance
-                    const clickListener = function(ev) {
-                        ev.stopPropagation();
-                        ev.preventDefault();
-                        const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
-                        // const distanceMsg = calcDist(place.location.lat,place.location.lng)
-                        const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-                        if (el && el === ev.target) {
-                            const label = document.createElement('span');
-                            const container = document.createElement('div');
-                            container.setAttribute('id', 'place-dist');
-                            label.innerText = distanceMsg;
-                            container.appendChild(label);
-                            document.body.appendChild(container);
-                            
-                            setTimeout(() => {
-                                container.parentElement.removeChild(container);
-                            }, 1500);
-                        }
-                    };
-
-                    text.addEventListener('click', clickListener);
-                    //*/
                     scene.appendChild(text);
                 });
             })
