b738f937aa28a395c28998189b313a9cb5a18a1c
Lord-Kennus <kennedy-a40@ulster.ac.uk>
2022-07-21 03:43:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ window.onload = () => {
                     const text = document.createElement('a-link');
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     text.setAttribute('title', place.name);
-                    text.setAttribute('scale', '13 13 13');
+                    text.setAttribute('scale', '10 10 10');
 
                     text.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
