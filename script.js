b125e1dddccc3b46f66bce6e50ea8ee817dc49a5
Lord-Kennus <kennedy-a40@ulster.ac.uk>
2022-07-25 21:35:38 +0100

    Test

--- script.js
+++ script.js
@@ -1,68 +1,108 @@
-const loadPlaces = function (coords) {
-    return loadPlaceStatic();
+import LatLon from 'https://cdn.jsdelivr.net/npm/geodesy@2.2.1/latlon-spherical.min.js';
+
+export class CalcVR {
+    constructor() {
+        this.distance = 0;
+        this.bearing = 0;
+        this.newPosition = [0, 0];
+        this.currentPosition = [0, 0];
+        this.objectSize = '0, 0, 0';
+        this.newDistance = 800;
+    }
+    
+    calcDist(currentPosiArg, targetPosition) {
+        const current = new LatLon(currentPosiArg[0], currentPosiArg[1]);
+        const target = new LatLon(targetPosition[0], targetPosition[1]);
+        this.distance = current.distanceTo(target);
+        this.bearing = current.finalBearingTo(target)
+        this.currentPosition = currentPosiArg;
+    }
+    calcNewPosition(currentPosition, bearing, newTargetToDistance) {
+        const current = new LatLon(currentPosition[0], currentPosition[1]);
+        const calculatedlced = current.destinationPoint(newTargetToDistance, bearing);
+        this.newPosition = [calculatedlced.latitude, calculatedlced.longitude];
+    }
+    calcSizeDist(distance) {
+        if(distance <= 1000 && distance >= 500){
+            this.objectSize = '25 25 25';
+            this.newDistance = 800;
+        }else if(distance > 1000 && distance <= 8000) {
+            this.objectSize = '20 20 20';
+            this.newDistance = 800 + (distance/1000);
+        }else if(distance > 8000 && distance <= 16000) {
+            this.objectSize = '18 18 18';
+            this.newDistance = 800 + (distance/1000);
+        }else if(distance > 16000 && distance <= 20000) {
+            this.objectSize = '15 15 15';
+            this.newDistance = 800 + (distance/1000);
+        }else if(distance > 20000) {
+            this.objectSize = '10 10 10';
+            this.newDistance = 800 + (distance/1000);
+        }
+    }
+
+}
+window.onload = () => {
+    navigator.geolocation.getCurrentPosition(success, error, options);
 };
 
-// get the static places
-function loadPlaceStatic() {
-    const PLACES = [
+function staticLoadPlaces() {
+    return [
         {
-            name: 'Hole 1',
+            name: 'Hole 2',
             location: {
                 lat: 55.085435,
                 lng: -6.453691,  
             }
         },
         {
-            name: 'Bunk',
+            name: 'Bunker',
             location: {
                 lat: 55.085828,
                 lng: -6.452037,
             }
         },
     ];
-
-    return new Promise((resolve, reject) => {
-        try {
-            resolve(PLACES)
-        } catch (err) {
-            reject(err)
-        }
-    })
 }
 
-window.onload = () => {
-    const scene = document.querySelector('a-scene');
+function renderPlaces(places, pos) {
+    let scene = document.querySelector('a-scene');
+    var crd = pos.coords;
+    let cal = new CalcVR();
 
-    // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+        let name = place.name;
+        let modelName = place.modelName;
+        cal.calcDist([crd.latitude, crd.longitude], [latitude, longitude]);
+	console.log(`heading: ${ crd.heading }`);
+        cal.calcNewPosition(cal.currentPosition, cal.bearing, cal.newDistance);
+        cal.calcSizeDist(cal.distance);
+        let model = document.createElement('a-box');
+        model.setAttribute('material', 'color: red');
+        model.setAttribute('gps-entity-place', `latitude: ${cal.newPosition[0]}; longitude: ${cal.newPosition[1]};`);
+        model.setAttribute('scale', `${cal.objectSize}`);
 
-        // than use it to load from remote APIs some places nearby
-        loadPlaces(position.coords)
-            .then((places) => {
-                alert(position.coords.latitude + " : " + position.coords.longitude);
-                places.forEach((place) => {
-                    const latitude = place.location.lat;
-                    const longitude = place.location.lng;
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
 
-                    // add place name
-                    const text = document.createElement('a-link');
-                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    text.setAttribute('title', place.name);
-                    text.setAttribute('scale', '8 8 8');
-
-                    text.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
-
-                    scene.appendChild(text);
-                });
-            })
-    },
-        (err) => console.error('Error in retrieving position', err),
-        {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 27000,
-        }
-    );
-};
+        scene.appendChild(model);
+    });
+}
+var options = {
+    enableHighAccuracy: true,
+    timeout: 50000,
+    maximumAge: 0
+  };
+  
+function success(pos) {
+    let places = staticLoadPlaces();
+    renderPlaces(places, pos);
+}
+  
+function error(err) {
+   console.warn(`ERROR(${err.code}): ${err.message}`);
+   alert('Unable to capture current location.');
+ }
