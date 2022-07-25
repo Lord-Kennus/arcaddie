90a7b30dec9f0a5f56da5ec1883d3403ae169e95
Lord-Kennus <kennedy-a40@ulster.ac.uk>
2022-07-25 19:16:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 const loadPlaces = function (coords) {
     return loadPlaceStatic();
 };
-// ------------------------------------FIREBASE--------------------------------
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, child, update, remove, get } from "firebase/database";
-
-const firebaseConfig = {
-    databaseURL: "https://golfar-ce0b6-default-rtdb.europe-west1.firebasedatabase.app/",
-};
-
-const firebaseConfig = {
- apiKey: "AIzaSyDObYM5Byctz3vuREPMLS3QA0yTjyxCals",
- authDomain: "golfar-ce0b6.firebaseapp.com",
- databaseURL: "https://golfar-ce0b6-default-rtdb.europe-west1.firebasedatabase.app",
- projectId: "golfar-ce0b6",
- storageBucket: "golfar-ce0b6.appspot.com",
- messagingSenderId: "41881550667",
- appId: "1:41881550667:web:ebfe5e13c7eaf5256c72ce"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
 
 // get the static places
 function loadPlaceStatic() {
@@ -50,52 +30,21 @@ function loadPlaceStatic() {
     })
 }
 
-// ------------------------------------BUTTON FUNCTIONS--------------------------------
-/*
-function NextHole(){
-    //resetPlaces();
-    Count++;
-    if (Count > 18){  // Future change, Count <= Hole + if statement checking if it reads it
-        alert('You are finished!');
-        Count--;
-    }
-    else{
-        //  renderPlaces(places);
-    }
-    document.getElementById("field1").value = ('Hole ' + Count);
-}
-
-function PreviousHole(){
-    //scene.parentElement.removeChild(scene);
-    //resetPlaces();
-    Count--;
-    if(Count < 1){
-        alert('There is no previous hole!');
-        Count = 1;
-    }
-    else{
-       //renderPlaces(places);
-    }
-    document.getElementById("field1").value = ('Hole ' + Count);
-}*/
-
-
 window.onload = () => {
-    /*const dbref = ref(db);
-    get(child(dbref,"Gracehill Golf Course/Hole" + Count + "P")).then((snapshot) =>{
-        if (snapshot.exists()){
-            alert("Test");
-        }
-    });*/
-
     const scene = document.querySelector('a-scene');
+
+    // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
+
+        // than use it to load from remote APIs some places nearby
         loadPlaces(position.coords)
             .then((places) => {
                 alert(position.coords.latitude + " : " + position.coords.longitude);
                 places.forEach((place) => {
                     const latitude = place.location.lat;
                     const longitude = place.location.lng;
+
+                    // add place name
                     const text = document.createElement('a-link');
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                     text.setAttribute('title', place.name);
@@ -104,6 +53,7 @@ window.onload = () => {
                     text.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
                     });
+
                     scene.appendChild(text);
                 });
             })
